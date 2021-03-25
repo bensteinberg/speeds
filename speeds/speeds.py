3558462cbeae88ab9378e61c575e8e02a60791e8
Ben Steinberg <bsteinberg@law.harvard.edu>
2021-03-25 09:13:19 -0400

    Make list comprehension vars consistent, assert we have enough colors, remove a superfluous for loop

--- speeds/speeds.py
+++ speeds/speeds.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def speeds():
     with jsonlines.open(os.environ.get('SPEEDS', 'speeds.jsonl')) as reader:
-        objects = [obj for obj in reader]
+        objects = [o for o in reader]
 
     locations = set([o['source'] for o in objects])
     output = {}
@@ -21,8 +21,9 @@ def speeds():
         ('blue', 'lightblue'),
         ('black', 'gray'),
     ]
+    assert len(locations) < len(palette), 'Not enough color pairs'
     for idx, loc in enumerate(locations):
-        colors[loc] = {'download': palette[idx][0], 'upload': palette[idx][1]}
-    for location in locations:
-        output[location] = [o for o in objects if o['source'] == location]
+        colors[loc] = {'download': palette[idx][0],
+                       'upload': palette[idx][1]}
+        output[loc] = [o for o in objects if o['source'] == loc]
     return render_template('index.html', objects=output, colors=colors)
